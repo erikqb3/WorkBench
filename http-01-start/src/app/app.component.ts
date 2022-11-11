93d09b8af1e8654e3b5dc02193900f52ecda1834
erikqb3 <erikqb3@gmail.com>
2022-11-10 20:17:41 -0700

    http request current lesson: "Showing a Loading Indicator"

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post<{ name: string }>(
+        "https://making-http-requests-4f4a0-default-rtdb.firebaseio.com/post.json",
+        postData
+    )
+    .subscribe(responseData => { //subscribing to http request automatically provides response data
+      console.log(responseData)
+    })
+    ;
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http
+      // .get('https://making-http-requests-4f4a0-default-rtdb.firebaseio.com/post.json')
+      // .pipe(map((responseData: {[key: string]: Post}) => {
+        .get<{ [key: string]: Post }>('https://making-http-requests-4f4a0-default-rtdb.firebaseio.com/post.json')  
+        .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)){
+            postsArray.push({ ...responseData[key], id: key })
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+        this.loadedPosts = posts
+      })
   }
 }
